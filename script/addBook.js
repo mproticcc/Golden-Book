c852d2f9a433170c0c21cf48ea9c7064cee481d4
mproticc <mprotic11022rn@raf.rs>
2023-10-27 14:51:32 +0200

    app

--- script/addBook.js
+++ script/addBook.js
@@ -1,39 +1,39 @@
 
 const errorMessages = document.getElementsByClassName("errorMessage");
 
-for(let message of errorMessages) {
+for (let message of errorMessages) {
     message.classList.add("display-none");
 }
 
-let authors = ["William Shakespeare","William Faulkner","Henry James","Charles Dickens"];
+let authors = ["William Shakespeare", "William Faulkner", "Henry James", "Charles Dickens"];
 let authorsDropDown = document.getElementById("author");
 authorsDropDown.innerHTML += "<option value='Author'>Author</option>";
 
-for(let i = 0;i<authors.length;i++){
-    authorsDropDown.innerHTML += "<option value="+authors[i]+">"+authors[i]+"</option>";
+for (let i = 0; i < authors.length; i++) {
+    authorsDropDown.innerHTML += "<option value=" + authors[i] + ">" + authors[i] + "</option>";
 }
 
 
-let categories = ["General","History","Fantasy","Literary"];
+let categories = ["General", "History", "Fantasy", "Literary"];
 
 let categoriesDropDown = document.getElementById("categories");
 categoriesDropDown.innerHTML = "<option value='Categories'>Categories</option>";
-for(let i = 0;i<categories.length;i++){
-    categoriesDropDown.innerHTML += "<option value="+categories[i]+">"+categories[i]+"</option>";
+for (let i = 0; i < categories.length; i++) {
+    categoriesDropDown.innerHTML += "<option value=" + categories[i] + ">" + categories[i] + "</option>";
 }
 
-document.getElementById("applyCourseForm").addEventListener("submit", function(event) {
+document.getElementById("applyCourseForm").addEventListener("submit", function (event) {
     event.preventDefault();
     let isFormValid = true;
 
-    const imageURL = document.getElementById("imageURL"); 
+    const imageURL = document.getElementById("imageURL");
     const regexImageURL = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
-    ;
+        ;
     const isImageURLValid = regexImageURL.test(imageURL.value);
 
     toggleErrorMessage(!isImageURLValid, imageURL);
 
-    if(!isImageURLValid) {
+    if (!isImageURLValid) {
         isFormValid = false;
     }
     console.log("IMAGE");
@@ -41,17 +41,17 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
     console.log(isFormValid);
 
     let year = document.getElementById("year");
-    const regexYear = /^\d+$/ ; 
+    const regexYear = /^\d+$/;
 
     let isYearValid = regexYear.test(year.value);
 
-    if(year.value>2022){
+    if (year.value > 2022) {
         isYearValid = false;
     }
 
     toggleErrorMessage(!isYearValid, year);
 
-    if(!isYearValid) {
+    if (!isYearValid) {
         isFormValid = false;
     }
     console.log("year");
@@ -60,13 +60,13 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
 
 
     const title = document.getElementById("title");
-    const regexTitle = /^[A-Z][A-Za-z0-9\s]{10,50}$/ ; 
+    const regexTitle = /^[A-Z][A-Za-z0-9\s]{10,50}$/;
 
     const isTitlelValid = regexTitle.test(title.value);
 
     toggleErrorMessage(!isTitlelValid, title);
 
-    if(!isTitlelValid) {
+    if (!isTitlelValid) {
         isFormValid = false;
     }
     console.log("title");
@@ -74,13 +74,13 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
     console.log(isFormValid);
 
     const descriptoin = document.getElementById("descriptoin");
-    const regexDescriptoin = /^[A-Za-z0-9\s]{1,250}$/ ; 
+    const regexDescriptoin = /^[A-Za-z0-9\s]{1,250}$/;
 
     const isDescriptoinValid = regexDescriptoin.test(descriptoin.value);
 
     toggleErrorMessage(!isDescriptoinValid, descriptoin);
 
-    if(!isDescriptoinValid) {
+    if (!isDescriptoinValid) {
         isFormValid = false;
     }
     console.log("descriptoin");
@@ -88,18 +88,18 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
     console.log(isFormValid);
 
     let ISBN = document.getElementById("ISBN");
-    const regexISBN = /^\d+$/ ; 
+    const regexISBN = /^\d+$/;
 
     let isISBNValid = regexISBN.test(ISBN.value);
 
-    
+
     toggleErrorMessage(!isISBNValid, ISBN);
 
     const author = document.getElementById("author");
     const isAuthorNotValid = author.value == "Author";
     toggleErrorMessage(isAuthorNotValid, author);
 
-    if(isAuthorNotValid) {
+    if (isAuthorNotValid) {
         isFormValid = false;
     }
     console.log("author");
@@ -110,48 +110,48 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
     const isCategoriesNotValid = categories.value == "Categories";
     toggleErrorMessage(isCategoriesNotValid, categories);
 
-    if(isCategoriesNotValid) {
+    if (isCategoriesNotValid) {
         isFormValid = false;
     }
     console.log("categories");
     console.log(isCategoriesNotValid);
-    
+
     console.log(isFormValid);
 
-    if(isFormValid) {
-    const add = document.getElementById('add');
+    if (isFormValid) {
+        const add = document.getElementById('add');
 
-  function Book(url,title, author, year,isbn,categories,descriptoin) {
-    this.url = url;
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.categories = categories;
-    this.descriptoin = descriptoin;
-    this.isbn = isbn;
-  }
+        function Book(url, title, author, year, isbn, categories, descriptoin) {
+            this.url = url;
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.categories = categories;
+            this.descriptoin = descriptoin;
+            this.isbn = isbn;
+        }
 
-  function Libary() {
-    this.bookList = [];
-  }
+        function Libary() {
+            this.bookList = [];
+        }
 
-  Libary.prototype.addbook = function (book) {
-    this.bookList.push(book);
-    console.log(this.bookList);
-  };
+        Libary.prototype.addbook = function (book) {
+            this.bookList.push(book);
+            console.log(this.bookList);
+        };
 
-  Libary.prototype.clearFields = function () {
-    document.getElementById('imageURL').value = '';
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = 0;
-    document.getElementById('year').value = '';
-    document.getElementById('categories').value = 0;
-    document.getElementById('descriptoin').value = '';
-    document.getElementById('ISBN').value = '';
-  };
-  Libary.prototype.render = function (book) {
-    const row = document.createElement('tbody');
-    row.innerHTML = `
+        Libary.prototype.clearFields = function () {
+            document.getElementById('imageURL').value = '';
+            document.getElementById('title').value = '';
+            document.getElementById('author').value = 0;
+            document.getElementById('year').value = '';
+            document.getElementById('categories').value = 0;
+            document.getElementById('descriptoin').value = '';
+            document.getElementById('ISBN').value = '';
+        };
+        Libary.prototype.render = function (book) {
+            const row = document.createElement('tbody');
+            row.innerHTML = `
     // <div class="class1">
     //     <div class="class2">
     //         <h3>${book.title}</h3>
@@ -165,45 +165,44 @@ document.getElementById("applyCourseForm").addEventListener("submit", function(e
     //     </div>
     // </div>
   `;
-   document.getElementById("book-test").appendChild(row);
-  };
+            document.getElementById("book-test").appendChild(row);
+        };
 
-  let publicLibary = new Libary();
+        let publicLibary = new Libary();
 
-  add.addEventListener('click', function (e) {
-    const url = document.getElementById('imageURL').value;
-    const title =  document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const year =  document.getElementById('year').value;
-    const categories = document.getElementById('categories').value;
-    const descriptoin = document.getElementById('descriptoin').value;
-    const isbn = document.getElementById('ISBN').value;
+        add.addEventListener('click', function (e) {
+            const url = document.getElementById('imageURL').value;
+            const title = document.getElementById('title').value;
+            const author = document.getElementById('author').value;
+            const year = document.getElementById('year').value;
+            const categories = document.getElementById('categories').value;
+            const descriptoin = document.getElementById('descriptoin').value;
+            const isbn = document.getElementById('ISBN').value;
 
-    let book = new Book(url,title, author, year,isbn,categories,descriptoin);
+            let book = new Book(url, title, author, year, isbn, categories, descriptoin);
 
-    publicLibary.addbook(book);
-    // publicLibary.render(book);
-    publicLibary.clearFields();
-    
-  });
+            publicLibary.addbook(book);
+            // publicLibary.render(book);
+            publicLibary.clearFields();
+
+        });
 
         event.target.submit();
     }
 });
 
 function toggleErrorMessage(isFieldNotValid, formElement) {
-    if(isFieldNotValid) {
+    if (isFieldNotValid) {
         formElement.parentElement.querySelector(".errorMessage").classList.remove("display-none");
     } else {
         formElement.parentElement.querySelector(".errorMessage").classList.add("display-none");
     }
 }
 
-
 function myFunction() {
     var author = document.getElementById("author").value;
     document.getElementById("descriptoin").placeholder = author + " - Description";
-  }
+}
 
 
 
--- script/validatoin.js
+++ script/validatoin.js
@@ -1,41 +1,41 @@
 const errorMessages = document.getElementsByClassName("errorMessage"); // document.querySelectorAll(".errorMessage")
 
-for(let message of errorMessages) {
+for (let message of errorMessages) {
     message.classList.add("display-none");
 }
 
-document.getElementById("applyCourseForm").addEventListener("submit", function(event) {
+document.getElementById("applyCourseForm").addEventListener("submit", function (event) {
     event.preventDefault();
     let isFormValid = true;
 
-    const email = document.getElementById("email"); 
-    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+    const email = document.getElementById("email");
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const isEmailValid = regexEmail.test(email.value);
 
     toggleErrorMessage(!isEmailValid, email);
 
-    if(!isEmailValid) {
+    if (!isEmailValid) {
         isFormValid = false;
     }
 
     const password = document.getElementById("password");
-    const regexPass = /([A-Za-z]){8,}\w+/ ; 
+    const regexPass = /([A-Za-z]){8,}\w+/;
 
     const isPasslValid = regexPass.test(password.value);
 
     toggleErrorMessage(!isPasslValid, password);
 
-    if(!isPasslValid) {
+    if (!isPasslValid) {
         isFormValid = false;
     }
 
-    if(isFormValid) {
+    if (isFormValid) {
         event.target.submit();
     }
 });
 
 function toggleErrorMessage(isFieldNotValid, formElement) {
-    if(isFieldNotValid) {
+    if (isFieldNotValid) {
         formElement.parentElement.querySelector(".errorMessage").classList.remove("display-none");
     } else {
         formElement.parentElement.querySelector(".errorMessage").classList.add("display-none");
